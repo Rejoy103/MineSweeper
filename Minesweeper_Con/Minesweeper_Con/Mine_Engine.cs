4d7b1c24f610332fd4a0de5d9bc630f013b8c988
Rejoy103 <79836681+Rejoy103@users.noreply.github.com>
2021-03-01 17:17:05 +0800

    Add files via upload

---
+++ Minesweeper_Con/Minesweeper_Con/Mine_Engine.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper_Con
+{
+    class Mine_Engine
+    {
+        private int Board_Size = 9;
+        private int Nmine= 10;
+        private char[,] Board;
+        private char[,] Vis_Board;
+        private int[,] Mine;
+
+        public Mine_Engine()
+        {
+            
+        }
+        public void InitializeGame()
+        {
+            Vis_Board = new char[Board_Size, Board_Size];
+            Board = new char[Board_Size, Board_Size];
+            Mine = new int[Nmine, 2];
+            for (int i = 0; i < Board_Size; i++)
+            {
+                for (int j = 0; j < Board_Size; j++)
+                {
+                    Vis_Board[i, j] = 'o';
+                    Board[i, j] = 'o';
+                }
+            }
+            for (int i = 0; i < Nmine; i++)
+            {
+                Random z = new Random(DateTime.Now.Millisecond);
+                int x = z.Next() % (Board_Size);
+                int y = z.Next() % (Board_Size);
+                if (IsValid(x, y) && Board[x, y] != '*')
+                {
+                    Board[x, y] = '*';
+                    Mine[i, 0] = x;
+                    Mine[i, 1] = y;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+        public string Print_MainPage()
+        {
+            string b = "";
+            b = "---------------------------------------------\nMine Sweeper V0.1\n---------------------------------------------\n";
+            b += "1.Play\n2.Settings\n3.Quit\n";
+            b += "---------------------------------------------";
+            return b;
+        }
+        public string Print_Options()
+        {
+            string b = "";
+            b = "---------------------------------------------\nOptions\n---------------------------------------------\n";
+            b += "1.Set Difficulty\n2.Set # of Mines\n3.Board Size\n4.Exit";
+            b+= "\n-------------------------------------------- - ";
+            return (b);
+        }
+        public string Print_Difficulty()
+        {
+            string b = "";
+            b = "---------------------------------------------\nDifficulty\n---------------------------------------------\n";
+            b += "1.Baby\n2.Kid\n3.Youth\n4.Adult";
+            b += "\n---------------------------------------------";
+            return b;
+        }
+        public void SetMine(int x)
+        {
+            Nmine = x;
+        }
+        public void SetSize(int x)
+        {
+            Board_Size = x;
+        }
+        public void SetDifficult(string o)
+        {
+            if (o == "1")
+            {
+                Nmine = 5;
+                Board_Size = 9;
+            }
+            if (o == "2")
+            {
+                Nmine = 10;
+                Board_Size = 9;
+            }
+            if (o == "3")
+            {
+                Nmine = 15;
+                Board_Size = 18;
+            }
+            if (o == "4")
+            {
+                Nmine = 30;
+                Board_Size = 20;
+            }
+        }
+        public Boolean Press(int x,int y,char mode)
+        {
+            if (Board[x, y] == '*' && mode=='u')
+            {
+                for(int i = 0; i < Nmine; i++)
+                {
+                    Vis_Board[Mine[i, 0], Mine[i, 1]] = '*';
+                }
+                return false;
+            }else if(Board[x,y]=='*' && mode == 'c')
+            {
+                return false;
+            }
+            else
+            {
+                Vis_Board[x, y] = '-';
+                Board[x, y] = '-';
+                int t = MineCount(x, y);
+                if (t > 0)
+                {
+                    Vis_Board[x, y] = t.ToString().ToCharArray()[0];
+                }
+                if (IsValid(x - 1, y - 1))
+                {
+                    int c = MineCount(x - 1, y - 1);
+                    if (Board[x - 1, y - 1] != '*' && Board[x - 1, y - 1] != '-' && c==0)
+                    {
+                        Press(x - 1, y - 1, 'c');
+                    }
+                    else if(c !=0 && Board[x - 1, y - 1] != '*')
+                    {
+                        Vis_Board[x - 1, y - 1] = c.ToString().ToCharArray()[0];
+                        Board[x - 1, y - 1] = '-';
+                    }
+                    else
+                    {
+                    }
+                }
+                if (IsValid(x - 1, y))
+                {
+                    int c = MineCount(x - 1, y);
+                    if (Board[x - 1, y] != '*' && Board[x - 1, y] != '-' && c==0)
+                    {
+                        Press(x - 1, y, 'c');
+                    }
+                    else if(c !=0 && Board[x - 1, y] != '*')
+                    {
+                        Vis_Board[x - 1, y] = c.ToString().ToCharArray()[0];
+                        Board[x - 1, y ] = '-';
+                    }
+                    else
+                    {
+                       
+                    }
+                }
+                if (IsValid(x - 1, y + 1))
+                {
+                    int c = MineCount(x - 1, y + 1);
+                    if (Board[x - 1, y + 1] != '*' && Board[x - 1, y + 1] != '-' && c==0)
+                    {
+                        Press(x - 1, y + 1, 'c');
+                    }
+                    else if(c!=0 && Board[x - 1, y + 1] != '*')
+                    {
+                        Vis_Board[x - 1, y + 1] = c.ToString().ToCharArray()[0];
+                        Board[x - 1, y + 1] = '-';
+                    }
+                    else
+                    {
+                      
+                    }
+                }
+                if (IsValid(x, y - 1))
+                {
+                    int c = MineCount(x, y - 1);
+                    if (Board[x, y - 1] != '*' && Board[x, y - 1] != '-' && c==0)
+                    {
+                        Press(x, y - 1, 'c');
+                    }
+                    else if(c!=0 && Board[x, y - 1] != '*')
+                    {
+                        Vis_Board[x, y - 1] = c.ToString().ToCharArray()[0];
+                        Board[x , y - 1] = '-';
+                    }
+                    else
+                    {
+                       
+                    }
+                }
+                if (IsValid(x, y + 1))
+                {
+                    int c = MineCount(x, y + 1);
+                    if (Board[x, y + 1] != '*' && Board[x, y + 1] != '-' && c==0)
+                    {
+                        Press(x, y + 1, 'c');
+                    }
+                    else if(c !=0 && Board[x, y + 1] != '*')
+                    {
+                        Vis_Board[x, y + 1] = c.ToString().ToCharArray()[0];
+                        Board[x , y + 1] = '-';
+                    }
+                    else
+                    {
+                      
+                    }
+                }
+                if (IsValid(x + 1, y - 1))
+                {
+                    int c = MineCount(x + 1, y - 1);
+                    if (Board[x + 1, y - 1] != '*' && Board[x + 1, y - 1] != '-' && c==0)
+                    {
+                        Press(x + 1, y - 1, 'c');
+                    }
+                    else if(c!=0 && Board[x + 1, y - 1] != '*')
+                    {
+                        Vis_Board[x + 1, y - 1] = c.ToString().ToCharArray()[0];
+                        Board[x + 1, y - 1] = '-';
+                    }
+                    else
+                    {
+                     
+                    }
+                }
+                if (IsValid(x + 1, y))
+                {
+                    int c = MineCount(x + 1, y);
+                    if (Board[x + 1, y] != '*' && Board[x + 1, y] != '-' && c==0)
+                    {
+                        Press(x + 1, y, 'c');
+                    }
+                    else if(c!=0 && Board[x + 1, y] != '*')
+                    {
+                        Vis_Board[x + 1, y] = c.ToString().ToCharArray()[0];
+                        Board[x + 1, y] = '-';
+                    }
+                    else
+                    {
+                      
+                    }
+                }
+                if (IsValid(x + 1, y + 1))
+                {
+                    int c = MineCount(x + 1, y + 1);
+                    if (Board[x + 1, y + 1] != '*' && Board[x + 1, y + 1] != '-' && c==0)
+                    {
+                        Press(x + 1, y + 1, 'c');
+                    }
+                    else if(c!=0 && Board[x + 1, y + 1] != '*')
+                    {
+                        Vis_Board[x + 1, y + 1] = c.ToString().ToCharArray()[0];
+                        Board[x + 1, y + 1] = '-';
+                    }
+                    else
+                    {
+                     
+                    }
+                }
+            }
+            return true;
+        }
+        public Boolean CheckGameState()
+        {
+            int c=0;
+            for(int i = 0; i < Board_Size; i++)
+            {
+                for(int j = 0; j < Board_Size; j++)
+                {
+                    if (Board[i, j] == 'o')
+                    {
+                        c++;
+                    }
+                }
+            }
+            if (c == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean IsPlayed(int x,int y)
+        {
+            if (IsValid(x,y))
+            {
+                if (Board[x, y] == '-')
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public string printBoard()
+        {
+            string b = "  ";
+            for(int i = 0; i < Board_Size; i++)
+            {
+                b += (i+1).ToString() + " ";
+            }
+            b += "\n";
+            for(int i = 0; i < Board_Size; i++)
+            {
+                b += (i + 1).ToString() + " ";
+                for (int j = 0; j < Board_Size; j++)
+                {
+                    
+                    b += Vis_Board[i, j] + " ";
+                }
+                b += "\n";
+            }
+            return b;
+        }
+        public string printBoard_Mine()
+        {
+            string b = " 1 2 3 4 5 6 7 8 9\n";
+            for (int i = 0; i < Board_Size; i++)
+            {
+                b += (i + 1).ToString() + " ";
+                for (int j = 0; j < Board_Size; j++)
+                {
+
+                    b += Board[i, j] + " ";
+                }
+                b += "\n";
+            }
+            return b;
+        }
+        private int MineCount(int x,int y)
+        {
+            int count = 0;
+            if (IsValid(x - 1, y - 1))
+            {
+                if (Board[x - 1, y - 1] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x - 1, y))
+            {
+                if (Board[x - 1, y] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x - 1, y + 1))
+            {
+                if (Board[x - 1, y + 1] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x, y - 1))
+            {
+                if (Board[x, y - 1] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x, y + 1))
+            {
+                if (Board[x, y + 1] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x + 1, y - 1))
+            {
+                if (Board[x + 1, y - 1] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x + 1, y))
+            {
+                if (Board[x + 1, y] == '*')
+                {
+                    count++;
+                }
+            }
+            if (IsValid(x + 1, y + 1))
+            {
+                if (Board[x + 1, y+1] == '*')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        public Boolean IsValid(int x,int y)
+        {
+            if((x<Board_Size && x>=0) && (y < Board_Size && y>=0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Minesweeper_Con/Minesweeper_Con/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Minesweeper_Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gamestate = true;
+            bool winstate = false;
+            Mine_Engine engine = new Mine_Engine();
+            while (gamestate)
+            {
+                Console.WriteLine(engine.Print_MainPage());
+                string n = Console.ReadLine();
+                switch (n)
+                {
+                    case "1":
+                        engine.InitializeGame();
+                        while (gamestate)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(engine.printBoard());
+                            Console.WriteLine("Enter location as (x y):");
+                            string x = Console.ReadLine();
+                            int x1 = Convert.ToInt32(x.Split(" ")[0]);
+                            int y1 = Convert.ToInt32(x.Split(" ")[1]);
+                            if (!engine.IsPlayed(x1 - 1, y1 - 1))
+                            {
+                                gamestate = engine.Press(x1 - 1, y1 - 1, 'u');
+                            }
+                            winstate = engine.CheckGameState();
+                            if (winstate)
+                            {
+                                break;
+                            }
+                        }
+                        if (winstate)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You Have Won the Game");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine(engine.printBoard());
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine(engine.Print_Options());
+                        string r = Console.ReadLine();
+                        switch (r)
+                        {
+                            case "1":
+                                Console.Clear();
+                                Console.WriteLine(engine.Print_Difficulty());
+                                string rn = Console.ReadLine();
+                                engine.SetDifficult(rn);
+                                Console.Clear();
+                                engine.Print_Options();
+                                break;
+                            case "2":
+                                Console.Clear();
+                                Console.WriteLine("Enter the Number of Mines Needed : ");
+                                string temp = Console.ReadLine();
+                                int n1 = Convert.ToInt32(temp);
+                                engine.SetMine(n1);
+                                Console.Clear();
+                                engine.Print_Options();
+                                break;
+                            case "3":
+                                Console.Clear();
+                                Console.WriteLine("Enter the Board Size : ");
+                                string temp1 = Console.ReadLine();
+                                int n2 = Convert.ToInt32(temp1);
+                                engine.SetSize(n2);
+                                Console.Clear();
+                                engine.Print_Options();
+                                break;
+                            case "4":
+                                break;
+                        }
+                        break;
+                    case "3":
+                        gamestate = false;
+                        break;
+                }
+            }
+            
+            
+
+        }
+    }
+}
